9da58e3222fa9d0f2a35dfce4385863c9dbca0b6
Dora Budić <dbudic0706@gmail.com>
2021-03-19 16:54:16 +0100

    Half part2, finished part3

--- Cats/main.js
+++ Cats/main.js
@@ -1,4 +1,4 @@
-// Milestone 1
+// MILESTONE 1
 // Creare un array di oggetti
 const cats = [
   {
@@ -37,3 +37,43 @@ const cats = [
 cats.forEach((item) => {
   $('#cats').append(`<li>${item.name} has ${item.color} fur.</li>`)
 });
+
+// MILESTONE 2
+// Dividere gli oggetti in 2 array in base al sesso, aggiungere a fianco un fiocco colorato
+const male = cats.filter((item) => item.sex == 'Male');
+const female = cats.filter((item) => item.sex == 'Female');
+
+male.forEach((item) => {
+  if (item.age < 10) {
+    item['opacity'] = item.age / 10;
+  } else {
+    item['opacity'] = 1;
+  }
+  $('#male').append(`<li>${item.name}<i class="fas fa-ribbon"></i></li>`);
+});
+
+female.forEach((item) => {
+  if (item.age < 10) {
+    item['opacity'] = item.age / 10;
+  } else {
+    item['opacity'] = 1;
+  }
+  $('#female').append(`<li>${item.name}<i class="fas fa-ribbon"></i></li>`)
+});
+
+// MILESTONE 3
+// Creo array con prima i gatti femmina e poi i gatti maschio
+const allCats = [...female,...male];
+
+// Nel nuovo array inserisco solo nome, colore e opacity
+const newCats = allCats.map((element) => {
+  let {name, color, opacity} = element;
+  let obj = {
+    name,
+    color,
+    opacity
+  }
+  return obj;
+});
+
+console.log(newCats);
